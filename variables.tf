41d64a0b8ecf2d1be92ec0b9f1db911d9b3103bc
arazzaq01 <razzaq.atiif@gmail.com>
2024-11-12 00:41:52 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-# Define variables for AWS account IDs
 variable "aws_1_0_account_id" {
   description = "AWS 1.0 Account ID where the default event bus resides"
   type        = string
@@ -9,43 +8,12 @@ variable "aws_2_0_account_id" {
   type        = string
 }
 
-# IAM Role to allow EventBridge rule in AWS 2.0 to send events to AWS 1.0
-resource "aws_iam_role" "eventbridge_role" {
-  name = "EventBridgeRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "events.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
+variable "s3_bucket_name" {
+  description = "S3 bucket name in AWS 2.0 to be accessed by Connect Direct in AWS 1.0"
+  type        = string
 }
 
-# IAM Policy to allow putting events to the AWS 1.0 event bus
-resource "aws_iam_policy" "eventbridge_policy" {
-  name        = "EventBridgePolicy"
-  description = "Allows EventBridge rule to put events on the AWS 1.0 event bus"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = "events:PutEvents"
-        Resource = "arn:aws:events:${var.aws_1_0_account_id}:event-bus/default"
-      }
-    ]
-  })
-}
-
-# Attach the policy to the role
-resource "aws_iam_role_policy_attachment" "eventbridge_policy_attachment" {
-  role       = aws_iam_role.eventbridge_role.name
-  policy_arn = aws_iam_policy.eventbridge_policy.arn
+variable "kms_key_id" {
+  description = "KMS Key ID in AWS 2.0 for encryption, accessible by AWS 1.0 Connect Direct"
+  type        = string
 }
